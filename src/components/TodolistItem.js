8de1ffdbb76bad762081b591606e0ff69c44c669
“zojian” <hellozojian@gmail.com>
2018-07-18 23:49:51 +0800

    Allow modification

--- src/components/TodolistItem.js
+++ src/components/TodolistItem.js
@@ -4,21 +4,40 @@ import Checkbox from '@material-ui/core/Checkbox';
 export default class TodolistItem extends Component {
   constructor(props) {
     super(props);
-    this.state = { checkedClass:""}
+    this.state = {
+      checkedClass: "",
+      editable:false
+    }
   }
 
   handleChange = () => {
-      let checkedClass = this.props.info.hasCompleted==false?"":"checked";
-      this.setState({checkedClass});
-      this.props.onToggle(this.props.info);
-      
+    let checkedClass = this.props.info.hasCompleted == false
+      ? ""
+      : "checked";
+    this.setState({checkedClass});
+    this.props.onToggle(this.props.info);
+
   }
+
+  handleDoubleClick = ()=>{
+      this.setState({editable:true});
+  }
+
+  handleEnter = (event)=>{
+      if(event.keyCode == 13){
+        this.setState({editable:false});
+      }
+  }
+
   render() {
-    let checkedClass = this.props.info.hasCompleted==false?"":"checked";
+    let {editable} = this.state;
+    let {handleDoubleClick, handleEnter} = this;
+    let checkedClass = this.props.info.hasCompleted == false
+      ? ""
+      : "checked";
     return (
-      <li className={checkedClass}>
-        <Checkbox checked={this.props.info.hasCompleted} onChange={this.handleChange}/> 
-        {this.props.content}
+      <li className={checkedClass} contentEditable={editable} onDoubleClick={handleDoubleClick} onKeyDown={handleEnter}>
+        <Checkbox checked={this.props.info.hasCompleted} onChange={this.handleChange}/> {this.props.content}
       </li>
     );
   }
